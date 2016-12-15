deca1cccbbf8ce19731258796b9f7c7c4f84c705
Orcaray <haditzah@gmail.com>
2016-12-15 08:57:54 +0100

    API google maps

---
+++ app/src/main/java/com/example/adminportatil/cebanctortillas/FirstMapActivity.java
@@ -0,0 +1,31 @@
+package com.example.adminportatil.cebanctortillas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class FirstMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FirstMapFragment mFirstMapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+
+        mFirstMapFragment = FirstMapFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.map_container, mFirstMapFragment)
+                .commit();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
+
