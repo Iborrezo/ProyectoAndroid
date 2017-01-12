ed326be8cddd92a3d9414ea21b28e4015b402a6e
Iborrezo <iigor_borrezo@hotmail.com>
2017-01-12 17:53:28 +0100

    Clase Pedido terminada

--- app/src/main/java/com/example/adminportatil/cebanctortillas/Bebidas.java
+++ app/src/main/java/com/example/adminportatil/cebanctortillas/Bebidas.java
@@ -1,6 +1,7 @@
 package com.example.adminportatil.cebanctortillas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,10 +14,11 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Bebidas extends Activity{
+public class Bebidas extends Activity {
     void showToast(CharSequence msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
+
     private Spinner spBebida;
     private EditText edtCantidad;
     private Button btnAñadir;
@@ -33,18 +35,18 @@ public class Bebidas extends Activity{
         setContentView(R.layout.activity_bebidas);
 
         Bundle extra = getIntent().getExtras();
-        String infPers = extra.getString("infPers");
-        listaCompra = (ArrayList<Producto>)extra.getSerializable("ArrayProducto");
+        final String infPers = extra.getString("infPers");
+        listaCompra = (ArrayList<Producto>) extra.getSerializable("ArrayProducto");
 
-        spBebida =(Spinner) findViewById(R.id.spBebida);
-        edtCantidad=(EditText)findViewById(R.id.edtCantidad2);
-        btnAñadir=(Button) findViewById(R.id.btnAñadir2);
-        btnSalir=(Button) findViewById(R.id.btnSalir2);
-        btnSiguiente=(Button) findViewById(R.id.btnSiguiente3);
-        btnSiguiente=(Button) findViewById(R.id.btnSiguiente3);
-        txtPrecio=(TextView) findViewById(R.id.txtPrecio);
+        spBebida = (Spinner) findViewById(R.id.spBebida);
+        edtCantidad = (EditText) findViewById(R.id.edtCantidad2);
+        btnAñadir = (Button) findViewById(R.id.btnAñadir2);
+        btnSalir = (Button) findViewById(R.id.btnSalir2);
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente3);
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente3);
+        txtPrecio = (TextView) findViewById(R.id.txtPrecio);
 
-        String[] bebidas = {"Seleccione una Bebida","Cocacola", "Kas Limon", "Kas Naranja","Nestea", "Cerveza", "Agua" };
+        String[] bebidas = {"Seleccione una Bebida", "Cocacola", "Kas Limon", "Kas Naranja", "Nestea", "Cerveza", "Agua"};
         ArrayAdapter<String> adapBebidas = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, bebidas);
         spBebida.setAdapter(adapBebidas);
 
@@ -53,8 +55,7 @@ public class Bebidas extends Activity{
                 new AdapterView.OnItemSelectedListener() {
                     public void onItemSelected(
                             AdapterView<?> parent, View view, int position, long id) {
-                        txtPrecio.setText(prBebidas[position]+"€");
-
+                        txtPrecio.setText((prBebidas[position]) + "€ unidad");
 
 
                     }
@@ -64,8 +65,39 @@ public class Bebidas extends Activity{
                     }
                 });
 
+        btnAñadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                añadir();
+            }
+        });
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                next(infPers);
+            }
+        });
+
     }
 
+    private void añadir() {
+        String descripcion;
+        int cantidad;
+        double precio;
+        descripcion = spBebida.getSelectedItem().toString();
+        precio = prBebidas[spBebida.getSelectedItemPosition()];
+        cantidad = Integer.parseInt(edtCantidad.getText().toString());
+        listaCompra.add(new Producto(precio, cantidad, descripcion));
+        Toast toast1;
+        toast1 = Toast.makeText(getApplicationContext(), "Se ha añadido " + cantidad + " " + descripcion + "/s .", Toast.LENGTH_SHORT);
+        toast1.show();
 
-
+    }
+    private void next(String infPers){
+        Intent i = new Intent(this, Resumen.class);
+        i.putExtra("ArrayProducto", listaCompra);
+        i.putExtra("infPers", infPers);
+        startActivity(i);
+    }
 }
